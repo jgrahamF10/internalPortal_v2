a4de8cb14ba42f96490a66c0ec3341c64f356a90
jessegraham <26193445+mi1knc0okies@users.noreply.github.com>
2024-10-02 13:34:20 -0400

    prep for production

--- app/rentals/page.tsx
+++ app/rentals/page.tsx
@@ -4,33 +4,31 @@ import { ColumnDef } from "@tanstack/react-table"
 
 import { useState } from "react";
 import { rentals, Rentals } from "@/db/schema/tracker_db";
-import { db } from "@/db";
-import { desc, asc } from "drizzle-orm";
 import { getRentals } from "./rentalActions";
 
 async function getData(): Promise<Rentals[]> {
     const data = await getRentals();
-    console.log("data", data);
+    //console.log("data", data);
     return data;
 }
 
-export const columns: ColumnDef<Rentals>[] = [
-    {
-      accessorKey: "rentalAgreement",
-      header: "Rental Agreement",
-    },
-    {
-      accessorKey: "Driver",
-      header: "Driver",
-    },
-    {
-      accessorKey: "pickUpDate",
-      header: "Amount",
-    },
-  ]
-
 export default async function RentalDashboard() {
     const data = await getData();
+
+    const columns: ColumnDef<Rentals>[] = [
+        {
+          accessorKey: "rentalAgreement",
+          header: "Rental Agreement",
+        },
+        {
+          accessorKey: "Driver",
+          header: "Driver",
+        },
+        {
+          accessorKey: "pickUpDate",
+          header: "Amount",
+        },
+      ]
 
     return (
         <div className="container mx-auto py-10">
